25755b49580607281308489b36db37e5e3dee9ce
saniya <s.aubakirova@gmail.com>
2016-02-19 10:15:18 +0000

    changed gemfile

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "s.aubakirova@gmail.com",
-  :user_name            => "s.aubakirova@gmail.com",
-  :password             => "sanDan310!",
-  :authentication       => "yes",
-  :enable_starttls_auto => true
-}
